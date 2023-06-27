31e92cf9828ad4bf7d307985e904023b44332c14
Felipe B Druzian <felipebdruzian@gmail.com>
2023-06-27 12:47:54 -0300

    tela inicial com .kv

--- Prototypes/main.py
+++ Prototypes/main.py
@@ -1,27 +1,10 @@
 from kivy.core.window import Window
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivy.graphics import Color, Rectangle
 from datepicker import CalendarWidget
 
 class CalendarScreen(MDBoxLayout):
-    def __init__(self, **kwargs):
-        super(CalendarScreen, self).__init__(**kwargs)
-        self.orientation = 'vertical'
-        self.padding = "10dp"
-        
-        with self.canvas:
-            Color(0, 0, 0, 1)  # Cor de fundo preta
-            self.rect = Rectangle(pos=self.pos, size=self.size)
-
-        self.bind(pos=self.update_rect, size=self.update_rect)
-
-        calendar_widget = CalendarWidget()
-        self.add_widget(calendar_widget)
-
-    def update_rect(self, *args):
-        self.rect.pos = self.pos
-        self.rect.size = self.size
+    pass
 
 
 class MainApp(MDApp):
